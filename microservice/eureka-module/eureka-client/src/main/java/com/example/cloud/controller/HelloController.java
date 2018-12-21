0b70d4bd8322759229ad10cbfb21f0766960e2c4
huozhisheng <278853662@qq.com>
2018-12-21 18:11:31 +0800

    ribbon修改

--- microservice/eureka-module/eureka-client/src/main/java/com/example/cloud/controller/HelloController.java
+++ microservice/eureka-module/eureka-client/src/main/java/com/example/cloud/controller/HelloController.java
@@ -2,14 +2,11 @@ package com.example.cloud.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,17 +24,20 @@ public class HelloController {
 	@Autowired
 	private Registration registration;
 
-	@RequestMapping(value = "/hello")
-	public String index(@RequestBody HelloVO helloVO, HttpServletRequest req) {
-		String name = req.getParameter("name");
-		if (StringUtils.isEmpty(name)) {
-			name = helloVO.getName();
-		}
+	@RequestMapping(value = "/helloGet")
+	public String helloGet(String name) {
 		String serviceId = registration.getServiceId();
 		List<ServiceInstance> serviceInstances = client.getInstances(serviceId);
 		logger.info("/hello , host:" + serviceInstances.get(0).getHost() + " ," + " service_id:" + serviceId);
-		return "hello world:" + name;
-
+		return "get请求 : hello world " + name;
 	}
 
+	@RequestMapping(value = "/helloPost")
+	public String helloPost(@RequestBody HelloVO helloVO) {
+		String name = helloVO.getName();
+		String serviceId = registration.getServiceId();
+		List<ServiceInstance> serviceInstances = client.getInstances(serviceId);
+		logger.info("/hello , host:" + serviceInstances.get(0).getHost() + " ," + " service_id:" + serviceId);
+		return "post请求 : hello world " + name;
+	}
 }
--- microservice/ribbon-module/ribbon-consumer/src/main/java/org/ribbon/example/controller/ConsumerController.java
+++ microservice/ribbon-module/ribbon-consumer/src/main/java/org/ribbon/example/controller/ConsumerController.java
@@ -1,10 +1,13 @@
 package org.ribbon.example.controller;
 
+import java.net.URI;
+
 import org.ribbon.example.vo.HelloVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,23 +17,32 @@ public class ConsumerController {
 	@Autowired
 	RestTemplate restTemplate;
 
+	// GET请求
 	@RequestMapping(value = "/rconsumer4GetEntity", method = RequestMethod.GET)
-	public String consumer4GetEntity() {
-		return restTemplate.getForEntity("http://hello-service/hello?name={1}", String.class, "GetEntity").getBody();
+	public String consumer4GetEntity(@RequestParam String name) {
+		return restTemplate.getForEntity("http://hello-service/helloGet?name={1}", String.class, name).getBody();
 	}
 
 	@RequestMapping(value = "/rconsumer4GetObject", method = RequestMethod.GET)
 	public String consumer4GetObject() {
-		return restTemplate.getForObject("http://hello-service/hello?name={1}", String.class, "GetObject");
+		return restTemplate.getForObject("http://hello-service/helloGet?name={1}", String.class, "GetObject");
 	}
 
+	// POST请求
 	@RequestMapping(value = "/rconsumer4PostEntity", method = RequestMethod.POST)
 	public String consumer4PostEntity(@RequestBody HelloVO helloVO) {
-		return restTemplate.postForEntity("http://hello-service/hello", helloVO, String.class).getBody();
+		return restTemplate.postForEntity("http://hello-service/helloPost", helloVO, String.class).getBody();
 	}
 
 	@RequestMapping(value = "/rconsumer4PostObject", method = RequestMethod.POST)
 	public String consumer4PostObject(@RequestBody HelloVO helloVO) {
-		return restTemplate.postForObject("http://hello-service/hello", helloVO, String.class);
+		return restTemplate.postForObject("http://hello-service/helloPost", helloVO, String.class);
 	}
+
+	@RequestMapping(value = "/rconsumer4PostLocation", method = RequestMethod.POST)
+	public String consumer4PostLocation(@RequestBody HelloVO helloVO) {
+		URI uri = restTemplate.postForLocation("http://hello-service/helloPost", helloVO, String.class);
+		return uri.toString();
+	}
+
 }
