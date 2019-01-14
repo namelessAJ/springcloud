24d7b8e6a898ce73e60e99fd2e2f475ef334317f
huozhisheng <278853662@qq.com>
2019-01-14 18:43:49 +0800

    a

--- microservice/feign-module/feign-consumer/src/main/java/org/feign/consumer/FeignConsumerApplication.java
+++ microservice/feign-module/feign-consumer/src/main/java/org/feign/consumer/FeignConsumerApplication.java
@@ -3,9 +3,10 @@ package org.feign.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
 
-@EnableFeignClients
 @SpringCloudApplication
+@EnableFeignClients(defaultConfiguration = FeignClientsConfiguration.class)
 public class FeignConsumerApplication {
 
 	public static void main(String[] args) {
---
+++ microservice/gateway-modoule/api-gateway/src/test/java/org/api/gateway/ZuulApplicationTest.java
@@ -0,0 +1,38 @@
+package org.api.gateway;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ZuulApplicationTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ZuulApplicationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ZuulApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
