24adb76169d8bea427f7cdb944c17d1479406db5
huozhisheng <278853662@qq.com>
2019-01-15 17:43:33 +0800

    zuul 指定路由地址跳转

--- microservice/gateway-modoule/api-gateway/src/main/java/org/api/gateway/ZuulApplication.java
+++ microservice/gateway-modoule/api-gateway/src/main/java/org/api/gateway/ZuulApplication.java
@@ -1,11 +1,11 @@
 package org.api.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@SpringCloudApplication
+@SpringBootApplication
 public class ZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
